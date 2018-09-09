ea30a8e00dfd2bd7ad6fcf1e28d1dd3e64f12dde
haradayu <hoge@gmail.com>
2018-09-09 22:51:03 +0900

    change machine types

--- 10-create_vm.py
+++ 10-create_vm.py
@@ -4,10 +4,10 @@ machine_type = 'f1-micro'
 image = "conda3ml"
 
 
-for i in range(6):
+for i in range(1):
     instance_name = f"instance-{i}"
-    command = f"gcloud compute instances create {instance_name} --preemptible --machine-type {machine_type}\
-                --image {image}"
+    #command = f"gcloud compute instances create {instance_name} --preemptible --machine-type {machine_type} --image {image}"
+    command = f"gcloud compute instances create {instance_name} --preemptible --image {image} --custom-cpu 8 --custom-memory 7424MB"
     print(command)
     os.system(command)
     
